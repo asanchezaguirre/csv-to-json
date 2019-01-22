7dd06907d28602e86372c0a27f5c174ed8d32132
Adriana <s.aguirre.adriana@gmail.com>
2019-01-22 15:42:43 -0600

    final

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const hostname = '127.0.0.1';
-const port = 3001;
+const port = 3008;
 
 const fs = require('fs');
 
@@ -15,25 +15,27 @@ server.listen(port, hostname, () => {
 });
 
 
-//Convertir a Json
 const csvFilePath = './customer-data.csv'
+
 const csv = require('csvtojson');
+
 csv()
 	.fromFile(csvFilePath)
 	.then((jsonObj) => {
 		console.log("Converting to JSON...")
 
-		var json_Obj = JSON.stringify({ data: jsonObj });
-		fs.writeFile('./data.json', json_Obj, (err) => {
+		
+		fs.writeFile('./data.json', JSON.stringify({ data: jsonObj }), (err) => {
 			if(err) throw err;
 		});
 
 		fs.readFile('./data.json', 'utf8', (err, data) => {
-			if(err)throw err;
+			if(err) throw err;
+
 
 			var obj = JSON.parse(data)
 
-			obj.data.forEach(element => console.log(element.id))
+			obj.data.map(element => console.log(element.id))
 		})
    	
 	})
